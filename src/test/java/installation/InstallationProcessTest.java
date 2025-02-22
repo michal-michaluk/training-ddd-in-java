9c20539d8c123b844968c66dbff49c8cb6c423cf
redicapoj <166955094+redicapoj@users.noreply.github.com>
2025-02-22 18:49:36 +0100

    test(InstallationProcess): add tests when process is finished.
    - Add test method to prevent assigning of device after process is finished.
    - Add test method to prevent assigning of installer after process is finished.

--- src/test/java/installation/InstallationProcessTest.java
+++ src/test/java/installation/InstallationProcessTest.java
@@ -15,13 +15,35 @@ public class InstallationProcessTest {
 
     @BeforeEach
     void setUp() {
+        InstallationProcess.existingOrders.clear();
         process = new InstallationProcess(orderId);
     }
 
     @Test
-    void createNewInstallationProcessTest() {
+    void createSecondInstallationProcessTest() {
         IllegalStateException exception = assertThrows(IllegalStateException.class,
                 () -> new InstallationProcess(orderId));
+
         assertEquals("Work order already exists!", exception.getMessage());
+    }
+
+    @Test
+    void processFinishedAndAssignedNewDevice() {
+        process.finishProcess();
+
+        IllegalStateException exception = assertThrows(IllegalStateException.class,
+                () -> process.assignDevice("device-x"));
+
+        assertEquals("Device cannot be assigned. Process Finished!", exception.getMessage());
+    }
+
+    @Test
+    void processFinishedAndAssignedNewDevice() {
+        process.finishProcess();
+
+        IllegalStateException exception = assertThrows(IllegalStateException.class,
+                () -> process.assignInstaller("installer-007"));
+
+        assertEquals("Installer cannot be assigned. Process Finished!", exception.getMessage());
     }
 }
