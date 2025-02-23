af6aa0e28e4ca95c7ccb74f12f31778d114234e6
redicapoj <166955094+redicapoj@users.noreply.github.com>
2025-02-23 17:59:41 +0100

    chore(BootNotification): remove redundant constructor

--- src/main/java/devices/configuration/installation/BootNotification.java
+++ src/main/java/devices/configuration/installation/BootNotification.java
@@ -8,10 +8,6 @@ public record BootNotification(
         String vendor, String model,
         String serial, String firmware) {
 
-    public BootNotification(String deviceId) {
-        this(deviceId, null, null, null, null, null);
-    }
-
     public enum Protocols {
         IoT20, IoT16
     }
