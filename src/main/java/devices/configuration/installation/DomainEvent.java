a9618f21c4dbc737669b293b5463bb57160f54a5
redicapoj <166955094+redicapoj@users.noreply.github.com>
2025-02-25 14:02:52 +0100

    refactor(events): replace the workOrder argument with order id
    - Simplify emitting events to store only order id and changed data.

--- src/main/java/devices/configuration/installation/DomainEvent.java
+++ src/main/java/devices/configuration/installation/DomainEvent.java
@@ -5,15 +5,15 @@ import devices.configuration.device.Location;
 public interface DomainEvent {
     record ProcessCreated(WorkOrder workOrder) implements DomainEvent {}
 
-    record InstallerAssigned(WorkOrder workOrder, String installerId) implements DomainEvent {}
+    record InstallerAssigned(String orderId, String installerId) implements DomainEvent {}
 
-    record DeviceAssigned(WorkOrder workOrder, String deviceId) implements DomainEvent {}
+    record DeviceAssigned(String orderId, String deviceId) implements DomainEvent {}
 
-    record BootNotificationReceived(WorkOrder workOrder, BootNotification pendingBootNotification) implements DomainEvent {}
+    record BootNotificationReceived(String orderId, BootNotification pendingBootNotification) implements DomainEvent {}
 
-    record BootNotificationConfirmed(WorkOrder workOrder, BootNotification confirmedBootNotification)implements DomainEvent {}
+    record BootNotificationConfirmed(String orderId, BootNotification confirmedBootNotification)implements DomainEvent {}
 
-    record LocationChanged(WorkOrder workOrder, Location location) implements DomainEvent {}
+    record LocationChanged(String orderId, Location location) implements DomainEvent {}
 
-    record InstallationFinished(WorkOrder workOrder) implements DomainEvent {}
+    record InstallationFinished(String orderId) implements DomainEvent {}
 }
